3a2facf6b20d812f1ac0dfd6c12555de42fee622
Lucian Bellini <lucianbellini1@gmail.com>
2023-04-20 22:10:32 -0300

    Adding the main files for frontend

---
+++ src/components/pokeCard/index.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+
+export default function MultiActionAreaCard({ name }: any) {
+  return (
+    <Card sx={{ maxWidth: 345, marginRight: "1em", marginBottom: "1em" }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
